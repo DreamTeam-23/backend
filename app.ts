92a5a216a725eb99f3a3a4d1f2d55bec8757dec0
Paul <spkim0921@gmail.com>
2024-03-08 18:09:02 -0800

    3rd commit
    set up app

---
+++ app.ts
@@ -0,0 +1,26 @@
+
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 3333
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("welcome")
+})
+
+async function start() {
+    try {
+        app.listen(port, () => console.log(`Server listening on port: ${port}`));
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+start()
