4d58c43f0502b20da84738273e257daab2c92bc2
harmanbir24 <harmanbir_19@hotmail.com>
2024-03-09 12:35:06 -0800

    added helper function to update the streaks once every day

--- helpers/streaksHelper.ts
+++ helpers/streaksHelper.ts
@@ -1,5 +1,28 @@
+import { Streak } from '../models/Streak'
 
+const updateStreaks = async () => {
+    const userStreaks = await Streak.find()
+    const currentDate = new Date()
 
-// FUNCTION incrementStreak
+    userStreaks.forEach(async (userStreak) => {
+        const lastUpdate = new Date(userStreak.updateDate)
+        const timeDiff = Math.abs(currentDate.getTime() - lastUpdate.getTime())
+        const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24))
 
-// FUNCTION resetStreak
+        if (dayDiff > 1) {
+            userStreak.currentStreak = 0
+        }
+        else {
+            userStreak.currentStreak += 1
+            if (userStreak.currentStreak > userStreak.longestStreak) {
+                userStreak.longestStreak = userStreak.currentStreak
+            }
+        }
+
+        userStreak.updateDate = currentDate
+
+        await userStreak.save()
+    })
+}
+
+export { updateStreaks }
