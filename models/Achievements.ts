6324091247f50ad3c58a8e15023945ac507a25fb
Ebod Shojaei <ebod.shojaei@gmail.com>
2024-03-09 13:07:50 -0800

    Revised achievements model

--- models/Achievements.ts
+++ models/Achievements.ts
@@ -1,5 +1,6 @@
 /*
 author: Harman
+collaborator: Ebod Shojaei
 date: March 9, 2024
 version: 1.0
 description: Achievements model schema for Space web server
@@ -8,30 +9,13 @@ description: Achievements model schema for Space web server
 import mongoose from "mongoose";
 
 const AchievementSchema = new mongoose.Schema({
-    achievmentId: { type: Number, required: [true, 'achievementId is required'] },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     name: { type: String, required: [true, 'name is required'] },
     description: { type: String, required: [true, 'description is required'] },
     criteria: { type: Number, required: [true, 'criteria is required'] },
     reward: { type: Number, required: [true, 'reward is required'] },
-    createDate: { type: Date, required: true, default: Date.now },
-})
-
-const UserAchievementSchema = new mongoose.Schema({
-    userId: { type: Number, required: [true, 'userId is required'] },
-    achievementId: { type: Number, required: [true, 'achievementId is required'] },
-    achieveDate: { type: Date, required: true, default: Date.now },
-})
-
-const RewardPatchesSchema = new mongoose.Schema({
-    rewardId: { type: Number, requried: [true, 'rewardId is required'] },
-    name: { type: String, required: [true, 'name is required'] },
-    description: { type: String, required: [true, 'description is required'] },
-    image: { type: Image, required: [true, 'image is required'] },
-})
+    createDate: { type: Date, default: Date.now }
+});
 
 const Achievement = mongoose.models.Achievement || mongoose.model('Achievement', AchievementSchema);
-const UserAchievement = mongoose.models.UserAchievement || mongoose.model('UserAchievement', UserAchievementSchema);
-const RewardPatches = mongoose.models.RewardPatches || mongoose.model('RewardPatches', RewardPatchesSchema);
-
-export { Achievement, UserAchievement, RewardPatches }
-
+export default Achievement;
