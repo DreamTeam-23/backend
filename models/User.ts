29aaa9bacd79551650e28ba8d561f7f08fe73f7d
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-03-08 22:27:07 -0800

    added user and event models

--- models/User.ts
+++ models/User.ts
@@ -1,12 +1,22 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: [true, 'username is required'], trim: true, maxlength: [80, 'username char limit is 80'] },
-    password: { type: String, required: [true, 'password is required'], maxlength: [80, 'password char limit is 80'] },
-    createDate: { type: Date, required: true, default: Date.now },
-    active: { type: Boolean, required: true, default: true },
-    userId: { type: Number, required: [true, 'userId is required'] }
-})
+const userSchema = new Schema({
+  UserID: Schema.Types.ObjectId,
+  FirstName: { type: String, required: true },
+  LastName: { type: String, required: true },
+  Email: { type: String, required: true, unique: true },
+  Password: { type: String, required: true },
+  University: String,
+  Major: String,
+  Role: { type: String, required: true },
+  CreatedAt: { type: Date, default: Date.now },
+  Events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+  Routine: {
+    SleepingHoursGoal: Number,
+    EatingReminder: Boolean,
+    EatingHabits: Number
+  }
+});
 
-const User = mongoose.models.User || mongoose.model('User', UserSchema)
-export default User
+module.exports = mongoose.model('User', userSchema);
