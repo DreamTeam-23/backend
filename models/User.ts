b1d845bd3250e22a42fe05299d39425d88c2668f
Ebod Shojaei <ebod.shojaei@gmail.com>
2024-03-09 13:49:05 -0800

    Updated user model and controller to use object ID. Added friends array to user model

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,7 @@
 
 /*
 author: Paul Kim
+collaborator: Ebod Shojaei
 date: March 9, 2024
 version: 1.0
 description: User model schema for Space web server
@@ -9,12 +10,16 @@ description: User model schema for Space web server
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    userId: { type: Number, required: [true, 'userId is required'] },
-    email: { type: String, required: [true, 'email is required'], maxlength: [255, 'email char limit is 255'] },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: [true, 'userId is required'], auto: true },
+    email: { type: String, required: [true, 'email is required'], unique: true, maxlength: [255, 'email char limit is 255'] },
     password: { type: String, required: [true, 'password is required'], maxlength: [80, 'password char limit is 80'] },
-    username: { type: String, required: [true, 'username is required'], trim: true, maxlength: [80, 'username char limit is 80'] },
+    username: { type: String, required: [true, 'username is required'], maxlength: [80, 'username char limit is 80'] },
+    reminders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reminder' }],
+    achievements: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Achievement' }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    streak: { type: Number, default: 0 },
     createDate: { type: Date, required: true, default: Date.now }
-})
+});
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
 export default User;
--- models/UserMessage.ts
+++ models/UserMessage.ts
@@ -9,10 +9,10 @@ description: user message model schema for Space API server
 import mongoose from "mongoose";
 
 export const UserMessageSchema = new mongoose.Schema({
+    userMessageId: { type: mongoose.Schema.Types.ObjectId, required: [true, 'userMessageId is required'], auto: true },
     email: { type: String, trim: true, maxlength: [255, 'title char limit is 80'] },
     content: { type: String, maxlength: [40000, 'content char limit is 10000'] },
-    createDate: { type: Date, required: true, default: Date.now },
-    userMessageId: { type: Number, required: [true, 'userMessageId is required'] }
+    createDate: { type: Date, required: true, default: Date.now }
 });
 
 const UserMessage = mongoose.models.UserMessage || mongoose.model('UserMessage', UserMessageSchema);
